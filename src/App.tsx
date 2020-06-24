7776d80f95f12ef1ddbc44a313036409a9723a96
pablo0178 <shestoe6@gmail.com>
2020-06-24 11:41:46 +0300

    Changed main page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       <Navbar />
       <div className="container">
         <Switch>
-          <Route component={TodoPage} path='/' exact />
+          <Route component={TodoPage} path='/React-projects' exact />
           <Route component={AboutPage} path='/about' />
         </Switch>
       </div>
