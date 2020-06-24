52b353d1bd61255e625296d0538693f1e0e85c62
pablo0178 <shestoe6@gmail.com>
2020-06-24 14:16:46 +0300

    Updates

---
+++ precache-manifest.d47cbdaf03e2c1d8ebe92b66a8e23a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff6e0639a7e29d41fa794a1f9bee026",
+    "url": "/react-io/index.html"
+  },
+  {
+    "revision": "478b4c871b36aaddfd11",
+    "url": "/react-io/static/css/main.a302fe3c.chunk.css"
+  },
+  {
+    "revision": "86d1920abe68e40e1a18",
+    "url": "/react-io/static/js/2.d3fb2918.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-io/static/js/2.d3fb2918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478b4c871b36aaddfd11",
+    "url": "/react-io/static/js/main.6316ce02.chunk.js"
+  },
+  {
+    "revision": "97df9bf99533bdab4786",
+    "url": "/react-io/static/js/runtime-main.03c0da40.js"
+  }
+]);
