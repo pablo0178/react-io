d4bf4ad4fc6775db5e59c953405cdf51d716f703
pablo0178 <shestoe6@gmail.com>
2020-06-24 12:44:45 +0300

    Changed page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       <Navbar />
       <div className="container">
         <Switch>
-          <Route component={TodoPage} path='/React' />
+          <Route component={TodoPage} path='/React' exact />
           <Route component={AboutPage} path='/about' />
         </Switch>
       </div>
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -5,9 +5,9 @@ export const Navbar: React.FunctionComponent = () => {
     return (
         <nav>
             <div className="nav-wrapper purple darken-3 px1">
-            <a href="/" className="brand-logo">React + Typesctipt</a>
+            <a href="/React" className="brand-logo">React+Typescript</a>
             <ul className="right hide-on-med-and-down">
-                <li><NavLink to="/">Список дел</NavLink></li>
+                <li><NavLink to="/React">Список дел</NavLink></li>
                 <li><NavLink to="/about">О нас</NavLink></li>
             </ul>
             </div>
